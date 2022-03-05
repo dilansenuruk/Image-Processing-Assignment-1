2b55ff606d97b02619c0fcb603582db2f16feb67
Dilan Senuruk <dilansenuruk@gmail.com>
2022-03-05 22:44:18 +0530

    codes for 1, 2, 3, 4, 5, 6, 7

--- code.py
+++ code.py
@@ -49,14 +49,14 @@ plt.show()
 img2 = cv.imread(r"C:\Users\User\Desktop\ML\images\brain_proton_density_slice.png", cv.IMREAD_GRAYSCALE)
 assert img2 is not None
 
-t1 = np.linspace(10, 10, 216)
-t2 = np.linspace(200, 200, 40)
-t3 = np.linspace(10, 10, 0)
+t1 = np.linspace(0, 10, 150)
+t2 = np.linspace(220, 255, 30)
+t3 = np.linspace(10, 10, 76)
 t = np.concatenate((t1, t2, t3), axis = 0).astype(np.uint8)
 
-r1 = np.linspace(10, 10, 186)
-r2 = np.linspace(200, 200, 30)
-r3 = np.linspace(10, 10, 40)
+r1 = np.linspace(0, 10, 185)
+r2 = np.linspace(220, 255, 30)
+r3 = np.linspace(10, 10, 41)
 r = np.concatenate((r1, r2, r3), axis = 0).astype(np.uint8)
 
 assert len(t) == 256
@@ -117,21 +117,22 @@ gamma = [0.2, 0.8, 1.2, 2.0]
 hist_img3_lab = []
 
 lab = cv.cvtColor(img3, cv.COLOR_BGR2LAB)
+lab_n = lab.copy()
 
 for i in gamma:
     k = np.array([(p/255)**(i)*255 for p in range(0, 256)]).astype(np.uint8)
     for x in range(0, len(lab)):
         for y in range(0, len(lab[0])):
-            lab[x, y][0] = ((lab[x, y][0] / 255)**(i)) * 255
+            lab_n[x, y][0] = ((lab[x, y][0] / 255)**(i)) * 255
     
-    hist_img3_lab.append(cv.calcHist([lab], [0], None, [256], [0, 256]))
+    hist_img3_lab.append(cv.calcHist([lab_n], [0], None, [256], [0, 256]))
     
     fig, ax = plt.subplots(1, 2, figsize = (12, 6))
     ax[0].plot(k)
     ax[0].set_title("L* Plane Curve (Gamma = " + str(i) + ")")
     ax[0].grid("on")
     
-    ax[1].imshow(cv.cvtColor(lab, cv.COLOR_LAB2RGB))
+    ax[1].imshow(cv.cvtColor(lab_n, cv.COLOR_LAB2RGB))
     ax[1].set_title("Gamma Corrected (Gamma = " + str(i) + ")")
     ax[1].axis("off")
 
@@ -180,6 +181,16 @@ def histogramEqualization(img4):
                 if (img4[i, j] == k):
                     l[k] += 1
                     break
+    
+    for i in range(0, 256, 1):
+        if (l[i] != 0):
+            low = i
+            break
+    
+    for i in range(255, -1, -1):
+        if (l[i] != 0):
+            high = i
+            break
 
     for i in range(1, 256):
         l[i] = l[i] + l[i - 1]
@@ -191,7 +202,7 @@ def histogramEqualization(img4):
         for j in range(img4.shape[1]):
             for k in range(256):
                 if (img4[i, j] == k):
-                    img4[i, j] = l[3*k]
+                    img4[i, j] = round((k - low)*((255 - 0)/(high - low)))
                     break
     return img4
 
@@ -208,6 +219,181 @@ ax[1, 0].set_title("Equalized Histogram")
 ax[1, 1].imshow(cv.cvtColor(img4_e, cv.COLOR_BGR2RGB))
 ax[1, 1].set_title("Histogram Equalized Image")
 ax[1, 1].axis("off")
+plt.show()
+ 
+
+
+
+
+
+
+#code for question (5)(a)------------------------------------------------------------------------------
+img5_1_small = cv.imread(r"C:\Users\User\Desktop\ML\images\a1q5images\im01small.png", cv.IMREAD_COLOR)
+img5_1 = cv.imread(r"C:\Users\User\Desktop\ML\images\a1q5images\im01.png", cv.IMREAD_COLOR)
+img5_2_small = cv.imread(r"C:\Users\User\Desktop\ML\images\a1q5images\im02small.png", cv.IMREAD_COLOR)
+img5_2 = cv.imread(r"C:\Users\User\Desktop\ML\images\a1q5images\im02.png", cv.IMREAD_COLOR)
+img5_3_small = cv.imread(r"C:\Users\User\Desktop\ML\images\a1q5images\im03small.png", cv.IMREAD_COLOR)
+img5_3 = cv.imread(r"C:\Users\User\Desktop\ML\images\a1q5images\im03.png", cv.IMREAD_COLOR)
+
+s = 4
+img_large = [img5_1, img5_2, img5_3]
+img_small = [img5_1_small, img5_2_small, img5_3_small]
+
+fig, ax = plt.subplots(3, 3, figsize = (18, 12))
+plt.subplots_adjust(wspace = None, hspace = -0.3)
+
+for k in range(3):
+    r = img_small[k].shape[0] * s
+    c = img_small[k].shape[1] * s
+    zoomed = np.zeros((r, c, 3), dtype = np.uint8)
+    for i in range(r):
+        for j in range(c):
+            zoomed[i, j] = img_small[k][round(i/s - 0.5), round(j/s - 0.5)]
+    
+    ax[k, 0].imshow(cv.cvtColor(img_small[k][125:150, 200:250], cv.COLOR_BGR2RGB))
+    ax[k, 0].set_title("Small")
+    ax[k, 1].imshow(cv.cvtColor(img_large[k][125*s:150*s, 200*s:250*s], cv.COLOR_BGR2RGB))
+    ax[k, 1].set_title("Zoomed")
+    ax[k, 2].imshow(cv.cvtColor(img_large[k][125*s:150*s, 200*s:250*s], cv.COLOR_BGR2RGB))
+    ax[k, 2].set_title("Large")
+
+
+
+
+
+
+#code for question (5)(b)------------------------------------------------------------------------------
+fig, ax = plt.subplots(3, 3, figsize = (18, 12))
+
+for k in range(3):
+    r = img_small[k].shape[0] * s
+    c = img_small[k].shape[1] * s
+    zoomed_im = np.zeros((r, c, 3), dtype = i)
+    for i in range(r-1):
+        for j in range(c-1):
+            e = i/s
+            f = j/s
+            
+            a = int(np.ceil(e))
+            b = int(np.floor(e))
+            c = int(np.ceil(f))
+            d = int(np.floor(f))
+            
+            lb = [b, d]
+            lt = [a, d]
+            rb = [b, c]
+            rt = [a, c]
+            
+            if (a >= img_small[k].shape[0]):
+                lt[0] = img_small[k].shape[0] - 1
+                rt[0] = img_small[k].shape[0] - 1
+            if (c >= img_small[k].shape[1]):
+                rb[1] = img_small[k].shape[1] - 1
+                rt[1] = img_small[k].shape[1] - 1
+                
+            rv = e-lb[0]
+            rh = f-lb[1]
+            val01 = img_small[k][lt[0]][lt[1]]*(rv) + img_small[k][lb[0]][lb[1]]*(1-rv)
+            val02 = img_small[k][rt[0]][rt[1]]*(rv) + img_small[k][rb[0]][rb[1]]*(1-rv)
+            zoomed_im[i][j] = np.rint(val01*(1-rh) + val02*(rh))
+                
+    ax[k, 0].imshow(cv.cvtColor(img_small[k][125:150, 200:250], cv.COLOR_BGR2RGB))
+    ax[k, 0].set_title("Small")
+    ax[k, 1].imshow(cv.cvtColor(zoomed_im[125*s:150*s, 200*s:250*s], cv.COLOR_BGR2RGB))
+    ax[k, 1].set_title("Zoomed")
+    ax[k, 2].imshow(cv.cvtColor(img_large[k][125*s:150*s, 200*s:250*s], cv.COLOR_BGR2RGB))
+    ax[k, 2].set_title("Large")
+
+
+
+
+
+
+#code for question (6)(a)------------------------------------------------------------------------------
+img6 = cv.imread(r"C:\Users\User\Desktop\ML\images\einstein.png", cv.IMREAD_GRAYSCALE).astype(np.float32)
+assert img6 is not None
+
+sobel_v= np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32)
+f_x = cv.filter2D(img6, -1, sobel_v)
+sobel_h = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32)
+f_y = cv.filter2D(img6, -1, sobel_h)
+grad_mag = np.sqrt(f_x**2 + f_y**2)
+
+fig, ax = plt.subplots(1, 2, figsize = (12, 6))
+
+ax[0].imshow(img6, cmap='gray', vmin=0, vmax=255)
+ax[0].set_title("Original")
+ax[0].axis("off")
+
+ax[1].imshow(grad_mag, cmap = "gray")
+ax[1].set_title("Gradient")
+ax[1].axis("off")
+
+plt.show()
+
+
+
+
+
+
+#code for question (6)(b)------------------------------------------------------------------------------
+m = img6.shape[0]
+n = img6.shape[1]
+im = np.zeros((m, n), np.uint8)
+for i in range(1, m-1):
+    for j in range(1, n-1):
+        G_x = img6[i-1,j-1]*(-1) + img6[i-1,j]*(-2) + img6[i-1,j+1]*(-1) + img6[i+1,j-1]*1 + img6[i+1,j]*2 + img6[i+1,j+1]*1
+        G_y = img6[i-1,j-1]*(-1) + img6[i,j-1]*(-2) + img6[i-1,j+1]*1 + img6[i+1,j-1]*(-1) + img6[i,j+1]*2 + img6[i+1,j+1]*1
+        val = np.sqrt(G_x**2 + G_y**2)
+        im[i, j] = (val / 1020) * 255
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+ax[0].imshow(img6, cmap = "gray")
+ax[0].set_title("Original")
+ax[0].axis("off")
+
+ax[1].imshow(im, cmap = "gray")
+ax[1].set_title("Sobel Edge Detected")
+ax[1].axis("off")
+
+plt.show()
+
+
+
+
+
+
+#code for question (6)(c)------------------------------------------------------------------------------
+img = np.zeros((m, n), np.uint8)
+
+mul = np.multiply((np.array([[1],[2],[1]], np.float32)), 
+                  (np.array([1,0,-1], np.float32)))
+sh = (-1) * mul
+sv = sh.T
+
+for i in range(1, m-1):
+    for j in range(1, n-1):
+        p = np.array([[img6[i-1,j-1],img6[i-1,j],img6[i-1,j+1]], 
+                      [img6[i,j-1],img6[i,j],img6[i,j+1]], 
+                      [img6[i+1,j-1],img6[i+1,j],img6[i+1,j+1]]])
+        mul_v = p * sv
+        mul_h = p * sh
+        cum_sum_v = np.sum(mul_v)
+        cum_sum_h = np.sum(mul_h)
+        val = np.sqrt(cum_sum_v**2 + cum_sum_h**2)
+        img[i, j] = (val / 1020) * 255
+                
+fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+
+ax[0].imshow(img6, cmap = "gray")
+ax[0].set_title("Original")
+ax[0].axis("off")
+
+ax[1].imshow(img, cmap = "gray")
+ax[1].set_title("Edge Detected")
+ax[1].axis("off")
+
 plt.show()
 
 
@@ -255,7 +441,7 @@ plt.show()
 
 
 
-#code for question (b)------------------------------------------------------------------------------
+#code for question (7)(b)------------------------------------------------------------------------------
 img7_blurred = cv.blur(img7_bgd, (9, 9), 2)
 img7_enhanced = cv.add(img7_blurred, img7_fgd)
 
